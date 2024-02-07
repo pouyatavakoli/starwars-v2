ad55ef574b394f33ede85973a3ad5e4189de2c5c
Pouya Tavakoli <69327223+pouyatavakoli@users.noreply.github.com>
2024-02-07 18:22:51 +0330

    added initial todos

---
+++ main.cpp
@@ -0,0 +1,22 @@
+// Pouya Tavakoli
+// 40212358011
+
+#include <iostream>
+using namespace std;
+
+// TODO : struct for saving game data
+// TODO : struct for defining enemy type details
+// TODO : struct for saving spaceship types details if needed
+// TODO : function to draw the map
+// TODO : function to move the spaceship
+// TODO : function to shoot
+// TODO : scoring system
+// TODO : function to refresh game environment
+// TODO : menu (pause resume change spaceship etc)
+// TODO : saving game data in file (delete the file when game ends)
+// TODO : win / lose logic 
+
+int main()
+{
+    return 0;
+}
