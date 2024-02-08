6575d4af3ce04de151be78793e35263a0946c567
Pouya Tavakoli <69327223+pouyatavakoli@users.noreply.github.com>
2024-02-08 11:18:30 +0330

    defined structures for each enemy type

--- main.cpp
+++ main.cpp
@@ -4,8 +4,41 @@
 #include <iostream>
 using namespace std;
 
-// TODO : struct for saving game data
-// TODO : struct for defining enemy type details
+// struct for saving game data
+struct GameData
+{
+    int ship_x;
+    // ship is always in the lowest row so  y = 0
+    int score = 0;
+    int heal = 3;
+};
+
+// struct for defining enemy type details
+// enemy size in map will be size*size
+
+struct Dart
+{
+    int size = 1;
+    int heal = 1;
+};
+
+struct Striker
+{
+    int size = 2;
+    int heal = 2;
+};
+
+struct Wraith
+{
+    int size = 3;
+    int heal = 4;
+};
+
+struct Banshee
+{
+    int size = 4;
+    int heal = 6;
+};
 // TODO : struct for saving spaceship types details if needed
 // TODO : function to draw the map
 // TODO : function to move the spaceship
@@ -14,7 +47,7 @@ using namespace std;
 // TODO : function to refresh game environment
 // TODO : menu (pause resume change spaceship etc)
 // TODO : saving game data in file (delete the file when game ends)
-// TODO : win / lose logic 
+// TODO : win / lose logic
 
 int main()
 {
